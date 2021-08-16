16537f475f7bdcf5a2fc0a4738c2570005e50995
moisesberrospi <mbf150@gmail.com>
2021-08-16 17:33:59 -0400

    Actualizando nombre de clases

--- src/PMTD/Establecer_enlace.java
+++ src/PMTD/Establecer_enlace.java
@@ -4,11 +4,11 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.*;
 import java.util.*;
 
-public class frm extends javax.swing.JFrame {
+public class Establecer_enlace extends javax.swing.JFrame {
 
      List<Soldado> lista = new ArrayList();
 
-    public frm() {
+    public Establecer_enlace() {
         initComponents();
     }
 
@@ -344,21 +344,23 @@ public class frm extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Establecer_enlace.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Establecer_enlace.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Establecer_enlace.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Establecer_enlace.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new frm().setVisible(true);
+                new Establecer_enlace().setVisible(true);
             }
         });
     }
